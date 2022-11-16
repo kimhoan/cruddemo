33ba3efc88c61ac81162a6e82a6df41239a654a6
kimhoan <hoan.nguyenkim94@gmail.com>
2022-11-16 22:14:42 +0700

    first commit

---
+++ todocrud/Screens/Detail.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import { firebase } from '../config';
+import { useNavigation } from '@react-navigation/native';
+
+const Detail = ({ route }) => {
+  const todoRef = firebase.firestore().collection('todos');
+  const [textHeading, onChangeHeadingText] = useState(
+    route.params.item.heading
+  );
+  const navigation = useNavigation();
+
+  const updateTodo = () => {
+    if (textHeading && textHeading.length > 0) {
+      todoRef
+        .doc(route.params.item.id)
+        .update({
+          heading: textHeading,
+        })
+        .then(() => {
+          // navigation.navigate('Home');
+          navigation.goBack();
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+        alert("Đã cập nhật mới");
+    }
+  };
+  const handlegoBack = () => {
+    navigation.navigate('Home');
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textfield}
+        onChangeText={onChangeHeadingText}
+        value={textHeading}
+        placeholder="Cập nhật"
+      />
+      <TouchableOpacity
+        style={styles.buttonUpdate}
+        onPress={() => {
+          updateTodo();
+        }}>
+        <Text>Cập Nhật</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonUpdate}
+        onPress={() => {
+          handlegoBack();
+        }}>
+        <Text>Quay về trang chủ</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 80,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  textfield: {
+    marginBottom: 10,
+    padding: 10,
+    fontSize: 15,
+    color: '#000000',
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+  },
+  buttonUpdate: {
+    marginTop: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 10,
+    backgroundColor: '#0de065',
+  },
+});
+
+export default Detail;
